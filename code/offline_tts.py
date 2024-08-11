1747fb12130c4aa7fd718a75b4e433fa4f16f271
Arabella Simpson <78235705+boatartist@users.noreply.github.com>
2024-08-11 12:42:44 +1000

    Update offline_tts.py

--- code/offline_tts.py
+++ code/offline_tts.py
@@ -1,9 +1,14 @@
 from time import sleep
 import os
+from gpiozero import OutputDevice
 
-def speak(text='Hello World'):
-    os.system(f'espeak "{text}" -s 120 -ven+f3 --stdout | aplay -D hw:2,0')
-    sleep(1)
+speaker = OutputDevice(24)
+speaker.off()
+def speak(text='Hello World', speaker_on=True):
+    if speaker_on:
+        speaker.on()
+    os.system(f'espeak "{text}" -s 120 -ven+f3 --stdout | aplay -D hw:1,0')
+    speaker.off()
     
 if __name__ == '__main__':
     speak()
