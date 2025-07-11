adec4d9579d0c4041df919a9e06d5bd9a00a5ba3
Arabella Simpson <78235705+boatartist@users.noreply.github.com>
2025-07-11 12:01:41 +1000

    Update servo.py with no jitter and correct angle setting

--- code/servo.py
+++ code/servo.py
@@ -1,17 +1,17 @@
 from gpiozero import AngularServo
 from time import sleep
-
-
+from gpiozero.pins.pigpio import PiGPIOFactory
+factory = PiGPIOFactory()
 
 class Servo:
     def __init__(self, pin):
-        self.servo = AngularServo(pin) #, min_pulse_width=0.0006, max_pulse_width=0.0023)
+        self.servo = AngularServo(pin, pin_factory=factory) #, min_pulse_width=0.0006, max_pulse_width=0.0023)
         self.angle = 0
         self.servo.angle = self.angle
     def correct_angle(self, angle):
-        if -90 <= self.angle-angle <= 90: 
-            self.angle = self.angle-angle
-        elif self.angle-angle < -90:
+        if -90 <= angle <= 90: 
+            self.angle = angle
+        elif angle < -90:
             self.angle = -90
         else:
             self.angle = 90
